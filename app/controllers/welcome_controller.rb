24ea1a2cd71b0d2632455c99250a57bfa20e20d1
Scott Kensell <skensell@gmail.com>
2018-04-05 16:06:38 -0700

    More analysis.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
+    months = Transaction.expenses.grouped_by_last_12_months.sum(:amount).zeros_removed
+    @months_involved = months.keys
+    @total_months = months.count
   end
 
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,21 @@
 class Transaction < ApplicationRecord
 
+  CATEGORIES_BUDGETED_YEARLY = ['Cats', 'Home Furnishing', 'Medical Bills', 'Plane tickets', 'Special Occasion']
+
   scope :expenses, -> {where(is_income: false)}
+  scope :budgeted_monthly, -> {where(is_budgeted_yearly: false)}
+  scope :budgeted_yearly, -> {where(is_budgeted_yearly: true)}
+  scope :grouped_by_last_12_months, -> {group_by_month(:date, last: 12)}
 
   def self.amount_spent_by_month
-    self.expenses.group_by_month(:date, last: 12).sum(:amount).zeros_removed
+    [
+        {name: 'All Categories', data: self.expenses.grouped_by_last_12_months.sum(:amount).zeros_removed},
+        {name: 'Monthly Categories', data: self.expenses.budgeted_monthly.grouped_by_last_12_months.sum(:amount).zeros_removed},
+    ]
+  end
+
+  def self.update_is_budgeted_yearly_column
+    self.where(category: CATEGORIES_BUDGETED_YEARLY).update_all(is_budgeted_yearly: true)
   end
 
 end
