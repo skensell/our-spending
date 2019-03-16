1b1eb140552c562a5606236308580c25f5719669
Scott Kensell <skensell@gmail.com>
2019-03-15 22:28:37 -0700

    Show month range.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -6,4 +6,13 @@ module WelcomeHelper
     number_to_currency(number, opts)
   end
 
+  def month_name(month_number)
+    months_enumerated.find { |name, index| index == month_number }.first
+  end
+
+  def months_enumerated
+    [['January', 1], ['February', 2], ['March', 3], ['April', 4], ['May', 5], ['June', 6],
+     ['July', 7], ['August', 8], ['September', 9], ['October', 10], ['November', 11], ['December', 12]]
+  end
+
 end
